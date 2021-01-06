898806917686deeb5b92c43f367fb3de7308c595
sidpar <sidpar@umich.edu>
2021-01-06 18:03:29 +0530

    Added instructions and the main function

--- tictactoe.py
+++ tictactoe.py
@@ -78,3 +78,49 @@ class Board:
             if self.matrix[0][i] == self.matrix[1][i] == self.matrix[2][i] == 'O':
                 return True
         return False
+
+
+def introduction():
+    print("""Welcome to
+    88888888888 8888888 .d8888b.       88888888888     d8888  .d8888b.       88888888888 .d88888b.  8888888888 
+        888       888  d88P  Y88b          888        d88888 d88P  Y88b          888    d88P" "Y88b 888        
+        888       888  888    888          888       d88P888 888    888          888    888     888 888        
+        888       888  888                 888      d88P 888 888                 888    888     888 8888888    
+        888       888  888                 888     d88P  888 888                 888    888     888 888        
+        888       888  888    888 888888   888    d88P   888 888    888 888888   888    888     888 888        
+        888       888  Y88b  d88P          888   d8888888888 Y88b  d88P          888    Y88b. .d88P 888        
+        888     8888888 "Y8888P"           888  d88P     888  "Y8888P"           888     "Y88888P"  8888888888
+    """)
+    print("-" * 80)
+    print("Here are a few instructions to get you started:")
+    print("A board looks like this:")
+    test_board = Board()
+    test_board.print_board()
+    print("You will be prompted to enter coordinates.")
+    print("Enter a row and column number (between 1 and 3) to place your move on the board.")
+    print("Enjoy playing Tic-Tac-Toe with your friends!")
+    print("-" * 80, end="\n\n\n")
+
+def main():
+    introduction()
+    board = Board()
+    board.print_board()
+
+    # Until the board is not solved, keep asking for moves and print board:
+    while not board.check_completion():
+        board.user_move()
+        board.print_board()
+        # Check if someone won:
+        if board.check_3_x():
+            print('X wins')
+            break
+        elif board.check_3_o():
+            print('O wins')
+            break
+    else:
+        print('Draw')
+
+
+if __name__ == '__main__':
+    main()
+
